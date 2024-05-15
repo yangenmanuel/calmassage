cfdac2a645340c7c8ba82dcc1e0e9974f985ce08
Yang Enmanuel <enmanuelcalzat@gmail.com>
2024-05-15 16:39:31 -0300

    added sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
-
+import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: 'https://calmassage.com.br',
+  integrations: [tailwind(), sitemap(), robotsTxt()]
 });
